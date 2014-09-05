24d1f8f9663d5f147d18bc3cfca88f91dd5df783
jfraiese <login.jfraiese@gmail.com>
2014-09-05 16:22:31 -0300

    Update Elemento.h

--- Elemento/Elemento.h
+++ Elemento/Elemento.h
@@ -18,4 +18,4 @@ typedef struct {
 bool ElementoMayor(const Elemento* a, const Elemento* b, TIPO_COMPARAR t);
 bool ElementoMenor(const Elemento* a, const Elemento* b, TIPO_COMPARAR t);
 bool ElementoIgual(const Elemento* a, const Elemento* b, TIPO_COMPARAR t);
-
+void ArmarString(const Elemento* a, char* s);
